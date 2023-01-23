4ed8227f8116190e24e86db52afae4d1ce68c598
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-01-23 17:41:38 -0600

    Eliminar recetas de localstorage y boton dinamico guardar-eliminar

--- js/app.js
+++ js/app.js
@@ -141,12 +141,14 @@ function iniciarApp(){
         //Botones de cerrar y favoritos 
         const btnFavorito = document.createElement(`button`);
         btnFavorito.classList.add(`btn`, `btn-danger`, `col`);
-        btnFavorito.textContent = "Guardar Favorito";
+        btnFavorito.textContent = existeStorage(idMeal) ? `Eliminar Favorito` : "Guardar Favorito";
 
         //boton local storage
         btnFavorito.onclick = function(){
             
             if(existeStorage(idMeal)){
+                eliminarFavorito(idMeal);
+                btnFavorito.textContent = "Guardar Favorito";
                 return
             }
 
@@ -155,6 +157,7 @@ function iniciarApp(){
                 titulo: strMeal,
                 img: strMealThumb
             });
+            btnFavorito.textContent = "Eliminar Favorito";
         }
 
         const btnCerrarModal = document.createElement(`button`);
@@ -180,6 +183,12 @@ function iniciarApp(){
     function existeStorage(id){
         const favoritos = JSON.parse(localStorage.getItem(`favoritos`)) ?? [];
         return favoritos.some(favorito => favorito.id === id);
+    }
+
+    function eliminarFavorito(id){
+        const favoritos = JSON.parse(localStorage.getItem(`favoritos`)) ?? [];
+        const nuevosFavoritos = favoritos.filter(favorito => favorito.id !== id);
+        localStorage.setItem(`favoritos`, JSON.stringify(nuevosFavoritos));
     }
 
     function limpiarHTML(selector){
